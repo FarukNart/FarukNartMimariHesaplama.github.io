1fe72d7f21ce4336ae33a8817e02abd49482d1a0
FarukNart <faruk.nart38@gmail.com>
2021-05-11 21:02:50 +0300

    FirstExample

---
+++ Eatery Free Website Template - Free-CSS.com/templatemo_515_eatery/js/malzeme.js
@@ -0,0 +1,21 @@
+/* Proje içerisinde kullanılacak olan malzeme ve fiyat değişkenleri */
+
+var alci = 19;
+var siva = 25;
+var ic_cephe_boya = 22;
+var dis_cephe_boya = 30;
+var tugla = 45;
+var beton = 150;
+var gaz_beton = 60;
+var duvar_seramik = 70;
+var duvar_kagit = 85;
+var sap = 22;
+var parke = 65;
+var zemin_seramik = 95;
+var granit = 105;
+var alci_pan = 75;
+var alci_siva = 35;
+var tas_yünü = 70;
+var boya = 22;
+var pvc_lambiri = 35;
+
